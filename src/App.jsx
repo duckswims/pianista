485a6b597546d9a2cdd2d3c0eb0d5faefc1e5851
Jay <67296806+jdai01@users.noreply.github.com>
2025-09-19 18:32:51 +0200

    added sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Sidebar from "./components/sidebar/Sidebar";
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div className="container-fluid">
+        <div className="row" style={{ minHeight: "100vh" }}>
+          {/* Sidebar */}
+          <div className="col-3 p-0">
+            <Sidebar />
+          </div>
+
+          {/* Content */}
+          <div className="col-9">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { fetchApi } from "../../scripts/api";
+
+import './online-indicator.css'
+
+
+function Sidebar() {
+  const [apiStatus, setApiStatus] = useState(null);
+
+  useEffect(() => {
+    async function checkApi() {
+      const result = await fetchApi();
+      setApiStatus(!result?.error); // true if API is online
+    }
+    checkApi();
+  }, []);
+
+
+  return (
+    <div className="d-flex flex-column p-3 bg-light" style={{ minHeight: "100%" }}>
+      <h3 className="d-flex align-items-center">
+        Pianista API
+        <div className={`online-indicator ${apiStatus ? "online" : "offline"} ms-2`}>
+            <span className="blink"></span>
+        </div>
+      </h3>
+      
+
+    </div>
+  );
+}
+
+export default Sidebar;
