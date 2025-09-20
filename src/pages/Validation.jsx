18bf66e304ae702609298e656f68689033896601
Jay <67296806+jdai01@users.noreply.github.com>
2025-09-20 13:16:15 +0200

    POST Validate Pddl Match

--- src/pages/Validation.jsx
+++ src/pages/Validation.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ValidateProblemPlan from "../components/validation/validate-problem-plan/ValidateProblemPlan";
 import PostValidatePddl from "../components/validation/post-validate-pddl/PostValidatePddl";
+import PostValidatePddlMatch from "../components/validation/post-validate-pddl-match/PostValidatePddlMatch";
 
 function Validation() {
   const [activeTab, setActiveTab] = useState("validate-plan");
@@ -27,12 +28,21 @@ function Validation() {
             Post Validate PDDL
           </button>
         </li>
+        <li className="nav-item">
+          <button
+            className={`nav-link ${activeTab === "post-pddl-match" ? "active" : ""}`}
+            onClick={() => setActiveTab("post-pddl-match")}
+          >
+            Post Validate PDDL Match
+          </button>
+        </li>
       </ul>
 
       {/* Tab content */}
       <div>
         {activeTab === "validate-plan" && <ValidateProblemPlan />}
         {activeTab === "post-pddl" && <PostValidatePddl />}
+        {activeTab === "post-pddl-match" && <PostValidatePddlMatch />}
       </div>
     </div>
   );
